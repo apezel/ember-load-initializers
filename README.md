f76d9e9ce672b92457739ab318f50c791c370bdc
Arnaud PEZEL <arnaud@apyx.fr>
2015-09-02 18:59:30 +0200

    -

--- README.md
+++ README.md
@@ -4,6 +4,29 @@ Ember load initializers
 About
 -----
 
+This is a fork of the official Ember load initializers. Added auto discover of exported initializers inside of a service.
+
+```
+
+/* inside service/my-service.js */
+
+export default function() {
+
+    console.log("Hello from service");
+
+};
+
+Initializer = {
+  name: 'sayHello',
+  initialize: function(container, app) {
+    app.inject('route', 'sayHello', 'service:my-service');
+  }
+}
+
+
+export {Initializer};
+```
+
 Ember loadInitializers is a tiny package to autoload your initializer files in EAK and ember-cli.
 
 License
--- ember-load-initializers.js
+++ ember-load-initializers.js
@@ -6,7 +6,7 @@ define("ember/load-initializers",
 
     return {
       'default': function(app, prefix) {
-        var regex = new RegExp('^' + prefix + '\/((?:instance-)?initializers)\/');
+        var regex = new RegExp('^' + prefix + '\/((?:instance-)?(service|initializers))\/');
         var getKeys = (Object.keys || Ember.keys);
 
         getKeys(requirejs._eak_seen).map(function (moduleName) {
@@ -16,16 +16,19 @@ define("ember/load-initializers",
             };
           })
           .filter(function(dep) {
-            return dep.matches && dep.matches.length === 2;
+            return dep.matches && dep.matches.length >= 2;
           })
           .forEach(function(dep) {
+            
+            console.log(matches);
+            
             var moduleName = dep.moduleName;
 
             var module = require(moduleName, null, null, true);
             if (!module) { throw new Error(moduleName + ' must export an initializer.'); }
 
             var initializerType = Ember.String.camelize(dep.matches[1].substring(0, dep.matches[1].length - 1));
-            var initializer = module['default'];
+            var initializer = (dep.matches[3] === "service" && module.Initializer) ? module.Initializer:module['default'];
             if (!initializer.name) {
               var initializerName = moduleName.match(/[^\/]+\/?$/)[0];
               initializer.name = initializerName;
